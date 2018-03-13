81bf063e825af1005046a2a9f020f1fd974169bc
AkidSe <akidse@mail.ru>
2018-03-13 17:19:22 +0200

    [api/users] added search user in API

--- module/api/users.php
+++ module/api/users.php
@@ -10,7 +10,24 @@ switch($router->getAction())
 	}
 	else echo false;
 	break;
+	case 'search':
+	if(!empty($_POST))
+	{
+		if(isset($_POST['term']) && mb_strlen($_POST['term']) >= 3)
+		{
+			$result = Database::query("SELECT `user_id` FROM `users` WHERE `first_name` LIKE ? OR `last_name` LIKE ? OR `middle_name` LIKE ?",
+				['%'.$_POST['term'].'%', '%'.$_POST['term'].'%', '%'.$_POST['term'].'%']);
+			$jsonResult = array("results" => array());
+			foreach($result as $user)
+			{
+				$tempUser = new User($user['user_id']);
+				$jsonResult['results'][] = array('id' => $tempUser->get('user_id'), 'text' => $tempUser->getFullName());
+			}
 
+			echo json_encode($jsonResult);
+		}
+	}
+	break;
 	default:
 	echo false;
 	break;

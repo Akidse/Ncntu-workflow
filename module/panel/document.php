cf0573eb4801b37c635c06c9004071261fa34fed
AkidSe <akidse@mail.ru>
2018-05-29 08:21:16 +0300

    removed old document module

--- module/panel/document.php
+++
@@ -1,9 +0,0 @@
-<?php
-$scriptManager->add("module/departmentSelect");
-$scriptManager->add("module/dropzone");
-
-if(isset($_FILES) && $_FILES != null)print_r($_FILES);
-if(isset($_POST) && $_POST != null)print_r($_POST);
-$documentTypes = Database::query("SELECT * FROM `message_types`", [], Database::FETCH_ALL);
-
-$firstLevelDep = Database::query("SELECT * FROM `departments` WHERE `subdepartment_id` IS NULL");

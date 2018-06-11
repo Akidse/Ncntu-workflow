0a1c7334f58261c7f814e85bfc3d556aee093555
AkidSe <akidse@mail.ru>
2018-06-11 18:15:13 +0300

    [logs] added logs to admin panel

---
+++ module/admin/logs.php
@@ -0,0 +1,18 @@
+<?php
+function folderSize($dir)
+{
+    $size = 0;
+    foreach (glob(rtrim($dir, '/').'/*', GLOB_NOSORT) as $each) {
+        $size += is_file($each) ? filesize($each) : 0;
+    }
+    return $size;
+}
+Template::setTitle('Логи та статистика');
+Template::setBackButtonUrl($router->url('/', 'admin'));
+
+$countUsers = Database::query("SELECT COUNT(*) FROM `users`");
+$countDocuments = Database::query("SELECT COUNT(*) FROM `departments_documents`");
+$countDecrees = Database::query("SELECT COUNT(*) FROM `decrees`");
+$countPrescriptions = Database::query("SELECT COUNT(*) FROM `prescriptions`");
+$countFiles = count(scandir(PathManager::getRootDirectory().'/files/')) - 2;
+$folderSize = round(folderSize(PathManager::getRootDirectory().'/files/')/1024/1024, 3);

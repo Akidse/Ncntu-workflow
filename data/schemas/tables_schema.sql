7358ee7ebe216fb00ed36b6f3f00dcaaf04e0e27
root <root@akidse11.fvds.ru>
2018-06-24 03:26:36 -0400

    new database schema

--- data/schemas/tables_schema.sql
+++ data/schemas/tables_schema.sql
@@ -23,6 +23,7 @@ CREATE TABLE `decrees` (
   `time_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
 CREATE TABLE `departments` (
   `department_id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
@@ -30,6 +31,7 @@ CREATE TABLE `departments` (
   `public` tinyint(1) NOT NULL,
   `main` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `departments_backup` (
   `department_id` int(11) NOT NULL,
@@ -52,6 +54,7 @@ CREATE TABLE `departments_documents` (
   `time_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
 CREATE TABLE `departments_documents_versions` (
   `version_id` int(11) NOT NULL,
   `document_id` int(11) NOT NULL,
@@ -68,21 +71,23 @@ CREATE TABLE `departments_documents_versions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `departments_permissions` (
-  `permission_id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission_id` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  PRIMARY KEY (permission_id)
+  `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 INSERT INTO `departments_permissions` (`permission_id`, `name`, `description`) VALUES
 (1, 'allow_decree', 'Можливість створювати накази'),
 (2, 'allow_access_maindep', 'Зв\'язок з головним підрозділом');
+
 
 CREATE TABLE `departments_permitted` (
   `permit_id` int(11) NOT NULL,
   `department_id` int(11) NOT NULL,
   `permission_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `documents_requests` (
   `request_id` int(11) NOT NULL,
@@ -95,12 +100,14 @@ CREATE TABLE `documents_requests` (
   `date_done` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
 CREATE TABLE `documents_signs` (
   `sign_id` int(11) NOT NULL,
   `document_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `files` (
   `file_id` int(11) NOT NULL,
@@ -109,6 +116,7 @@ CREATE TABLE `files` (
   `user_id` int(11) NOT NULL,
   `addition_to` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `letters` (
   `letter_id` int(11) NOT NULL,
@@ -120,10 +128,13 @@ CREATE TABLE `letters` (
   `unread` tinyint(1) NOT NULL DEFAULT '1',
   `time_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 CREATE TABLE `message_types` (
   `type_id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `prescriptions` (
   `prescription_id` int(11) NOT NULL,
@@ -135,10 +146,13 @@ CREATE TABLE `prescriptions` (
   `public` tinyint(1) DEFAULT '0',
   `time_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 CREATE TABLE `roles` (
   `post_id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
@@ -146,21 +160,20 @@ CREATE TABLE `users` (
   `password_hash` varchar(255) NOT NULL,
   `first_name` varchar(64) NOT NULL,
   `middle_name` varchar(64) NOT NULL,
-  `last_name` varchar(64) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  `avatar` varchar(64) NOT NULL,
-  `department_id` int(11) NOT NULL,
+  `last_name` varchar(64) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `avatar` varchar(64) DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL,
   `groups` varchar(64) DEFAULT NULL,
   `decree_last_view` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `prescription_last_view` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `locale` varchar(8) NOT NULL DEFAULT 'uk_UA',
-  `results_per_page` tinyint(3) UNSIGNED NOT NULL DEFAULT '10'
+  `prescription_last_view` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `locale` varchar(8) DEFAULT 'uk_UA',
+  `results_per_page` tinyint(3) UNSIGNED DEFAULT '10'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users_groups` (
-  `group_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (group_id)
+  `group_id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `users_groups` (`group_id`, `name`) VALUES
@@ -182,6 +195,16 @@ CREATE TABLE `users_groups_permitted` (
   `permission_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `users_notifications` (
+  `notification_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `text` varchar(512) NOT NULL,
+  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `unread` tinyint(1) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 CREATE TABLE `users_resume` (
   `user_id` int(11) NOT NULL,
   `phone_number` varchar(32) DEFAULT NULL,
@@ -194,55 +217,43 @@ ALTER TABLE `archive_requests`
 
 ALTER TABLE `decrees`
   ADD PRIMARY KEY (`decree_id`);
-
 ALTER TABLE `departments`
   ADD PRIMARY KEY (`department_id`);
-
 ALTER TABLE `departments_backup`
   ADD PRIMARY KEY (`department_id`);
-
 ALTER TABLE `departments_documents`
   ADD PRIMARY KEY (`document_id`);
-
-
 ALTER TABLE `departments_documents_versions`
   ADD PRIMARY KEY (`version_id`);
-
+ALTER TABLE `departments_permissions`
+  ADD PRIMARY KEY (`permission_id`);
 ALTER TABLE `departments_permitted`
   ADD PRIMARY KEY (`permit_id`);
-
 ALTER TABLE `documents_requests`
   ADD PRIMARY KEY (`request_id`);
-
 ALTER TABLE `documents_signs`
   ADD PRIMARY KEY (`sign_id`);
-
 ALTER TABLE `files`
   ADD PRIMARY KEY (`file_id`);
-
 ALTER TABLE `letters`
   ADD PRIMARY KEY (`letter_id`);
-
 ALTER TABLE `prescriptions`
   ADD PRIMARY KEY (`prescription_id`);
-
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`post_id`);
-
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
-
+ALTER TABLE `users_groups`
+  ADD PRIMARY KEY (`group_id`);
 ALTER TABLE `users_groups_permissions`
   ADD PRIMARY KEY (`permission_id`);
-
 ALTER TABLE `users_groups_permitted`
   ADD PRIMARY KEY (`permit_id`);
-
+ALTER TABLE `users_notifications`
+  ADD PRIMARY KEY (`notification_id`);
 ALTER TABLE `users_resume`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `user_id` (`user_id`);
-
-
 ALTER TABLE `archive_requests`
   MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `decrees`
@@ -253,6 +264,9 @@ ALTER TABLE `departments_documents`
   MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `departments_documents_versions`
   MODIFY `version_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `departments_permissions`
+  MODIFY `permission_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 ALTER TABLE `departments_permitted`
   MODIFY `permit_id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `documents_requests`
@@ -269,10 +283,17 @@ ALTER TABLE `roles`
   MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users_groups`
+  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 ALTER TABLE `users_groups_permissions`
   MODIFY `permission_id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `users_groups_permitted`
   MODIFY `permit_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users_notifications`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `users_resume`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

f8ffb0ee0eb608aa585f3379219340043bf86b76
root <root@akidse11.fvds.ru>
2018-06-24 02:42:40 -0400

    fix for locales

--- lib/locale.php
+++ lib/locale.php
@@ -3,5 +3,6 @@ putenv('LC_ALL='.$profile->get('locale'));
 setlocale(LC_ALL, $profile->get('locale'));
 
 bindtextdomain($profile->get('locale'), "./locale");
+bind_textdomain_codeset($profile->get('locale'), 'UTF-8');
 
 textdomain($profile->get('locale'));
